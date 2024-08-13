b2b33125c6fc67efd5a88857814b1cecab86e9cd
RezaMahmoudi00 <reza.mahmoudi@nexasoft.io>
2024-08-13 17:05:43 +0300

    CategoryCard.jsx developed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import Navbar from './components/Navbar/Navbar'
 import Hero from './components/Hero/Hero'
+import Category from './components/Category/Category'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,6 +10,7 @@ function App() {
     <dev>
       <Navbar/>
       <Hero/>
+      <Category/>
     </dev>  )
 }
 
